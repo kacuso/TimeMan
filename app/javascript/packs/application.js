bdcc9ddb96881ce11ccb1887086ced00c182295e
Gabriel Datovo <gabrielmontalbini@gmail.com>
2019-10-25 12:42:40 -0200

    add logo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,29 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree
+//= require jquery3
+//= require popper
+//= require bootstrap
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 	get 'welcome/index'
-	get '/assets/images/logo'
-	get '/assets/images/facebook'
-	get '/assets/images/instagram'
-	get '/assets/images/twitter'
+
 	
 	resources :resource	
 
